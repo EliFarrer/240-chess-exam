22743d16a9e2c3c00d0f28931a557327ee6a120c
Eli Farrer <eli.farrer@gmail.com>
2025-01-27 11:30:06 -0700

    added explanation of purpose

--- shared/src/main/java/chess/Hopper.java
+++ shared/src/main/java/chess/Hopper.java
@@ -1,5 +1,6 @@
 package chess;
 
 public interface Hopper {
+    // an interface for hopping chess pieces like the knight and king
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPiece piece);
 }
