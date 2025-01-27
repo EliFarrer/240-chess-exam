f2a11b73c76fa2c299e87375aaea623ddff5c07d
Eli Farrer <eli.farrer@gmail.com>
2025-01-27 11:34:17 -0700

    added possible knight moves

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -7,6 +7,11 @@ public class KnightMovesCalculator implements Hopper{
 
     public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
         ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
+        int[][] deltas = new int[][] {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
+        for (int[] delta : deltas) {
+            var newPosition = new ChessPosition(position.getRow() + delta[0], position.getColumn() + delta[1]);
+            possibleMoves.add(new ChessMove(position, newPosition, null));
+        }
         return possibleMoves;
     }
 
