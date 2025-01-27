d7b4892d511067b8f0c8f377cf3bba7fba310c28
Eli Farrer <eli.farrer@gmail.com>
2025-01-27 11:45:20 -0700

    changed the Piece type to new position

--- shared/src/main/java/chess/Hopper.java
+++ shared/src/main/java/chess/Hopper.java
@@ -2,5 +2,5 @@ package chess;
 
 public interface Hopper {
     // an interface for hopping chess pieces like the knight and king
-    public boolean canMove(ChessBoard board, ChessPosition position, ChessPiece piece);
+    public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition);
 }
