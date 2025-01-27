23bac96e7f845c42c0f118b39ca32b13143df761
Eli Farrer <eli.farrer@gmail.com>
2025-01-27 11:29:17 -0700

    made Knight moves calculator to use in ChessPiece

---
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -0,0 +1,17 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KnightMovesCalculator implements Hopper{
+
+    public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
+        ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
+        return possibleMoves;
+    }
+
+
+    public boolean canMove(ChessBoard board, ChessPosition position, ChessPiece piece) {return true;}
+
+
+}
